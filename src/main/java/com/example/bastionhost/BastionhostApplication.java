38cb4e30be857e22e97c0045a78b1a33e33003dc
IMNU-S1mple <1105499862@qq.com>
2022-08-01 21:13:41 +0800

    First Commit

---
+++ src/main/java/com/example/bastionhost/BastionhostApplication.java
@@ -0,0 +1,13 @@
+package com.example.bastionhost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BastionhostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BastionhostApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bastionhost/BastionhostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bastionhost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BastionhostApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
