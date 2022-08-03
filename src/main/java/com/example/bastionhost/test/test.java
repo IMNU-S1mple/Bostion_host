12754dba6d9db4163b212edc55bafd7af99c3df6
yanqing <yanqing@isrc.iscas.ac.cn>
2022-08-03 20:03:54 +0800

    Subdomain test

---
+++ src/main/java/com/example/bastionhost/test/test.java
@@ -0,0 +1,51 @@
+package com.example.bastionhost.test;
+
+import org.junit.Test;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
+public class test {
+    @Test
+    public void Subdomain(){
+        File file = new File("src/main/resources/Subdomain/Subdomain.txt");
+        BufferedReader reader = null;
+        try {
+            System.out.println("以行为单位读取文件内容，一次读一整行：");
+            reader = new BufferedReader(new FileReader(file));
+            String tempString = null;
+            int line = 1;
+            // 一次读入一行，直到读入null为文件结束
+            while ((tempString = reader.readLine()) != null) {
+                // 显示行号
+                System.out.println("line " + line + ": " + tempString);
+                line++;
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e1) {
+                }
+            }
+        }
+
+
+
+
+    }
+
+    @Test
+    public void System(){
+        String property = System.getProperty("user.dir");
+        System.out.println(property);
+    }
+}
